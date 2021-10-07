f58d256c347fa5a01574315e1e1e7f21195b07ed
santiagogrr <santiagog94@gmail.com>
2021-10-07 12:19:33 +0200

    Added list of coordinates tests

--- Wall.py
+++ Wall.py
@@ -4,7 +4,9 @@ class Wall(object):
   x: int
   y: int
   
-  def __init__(self, coordinates, nbdoors=0):
-    assert(isinstance(coordinates, tuple)), "Coordinates parameter must be a tuple"
-    assert len(coordinates) == 2, 'There must be two coordinates'
+  def __init__(self, list_of_coordinates, nbdoors=0):
+    assert(isinstance(list_of_coordinates, list)), "Coordinates parameter must be a list"
+    assert len(list_of_coordinates) == 2, 'There must be two set of coordinates'
+    assert len(list_of_coordinates) == 2 and isinstance(list_of_coordinates([0,1], tuple)), 'Each coordinate must be a tuple'
+
     self.nbDoors = nbdoors
--- wall_test.py
+++ wall_test.py
@@ -6,19 +6,23 @@ import unittest
 class wall_test(unittest.TestCase):
 
     def test_good_coordinates_no_errors(self):
-        Wall((0,1))
+        Wall([(0,1),(0,1)])
 
     def test_error_raised_when_only_one_coordinate(self):
         with self.assertRaises(AssertionError):
-            Wall((0,))
+            Wall([(0,),(0,)])
     
     def test_error_raised_when_three_coordinates(self):
         with self.assertRaises(AssertionError):
-            Wall((0,1,2))
+            Wall([(0,1,2),(0,1,2)])
     
-    def test_error_raised_when_list_of_coordinates(self):
+    def test_error_raised_when_three_set_of_coordinates(self):
         with self.assertRaises(AssertionError):
-            Wall([0,1])
-
+            Wall([(0,1),(0,1),(0,1)])
+    
+    def test_error_raised_when_list_only_have_one_set_of_coordinates(self):
+        with self.assertRaises(AssertionError):
+            Wall([(0,1)])
+    
     if __name__ =='__main__':
         pass
