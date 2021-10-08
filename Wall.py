3ec5ca43753896f22cc809a48d57752a69e5718e
Sarah Issaad <sarah.issaad.etu@univ-lille.fr>
2021-10-08 14:01:50 +0200

    Assertions about coordinates at creation are now correct

--- Wall.py
+++ Wall.py
@@ -7,6 +7,7 @@ class Wall(object):
   def __init__(self, list_of_coordinates, nbdoors=0):
     assert(isinstance(list_of_coordinates, list)), "Coordinates parameter must be a list"
     assert len(list_of_coordinates) == 2, 'There must be two set of coordinates'
-    assert len(list_of_coordinates) == 2 and isinstance(list_of_coordinates([0,1], tuple)), 'Each coordinate must be a tuple'
+    assert len(list_of_coordinates[0]) == 2 and isinstance(list_of_coordinates[0], tuple), 'First coordinate must be a tuple'
+    assert len(list_of_coordinates[1]) == 2 and isinstance(list_of_coordinates[1], tuple), 'Second coordinate must be a tuple'
 
     self.nbDoors = nbdoors
