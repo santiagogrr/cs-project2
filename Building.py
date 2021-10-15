6ea69375e4ef1d2248fbf921bdf053e65fc7dca1
santiagogrr <santiagog94@gmail.com>
2021-10-15 15:27:11 +0200

    continued with building tests

--- Building.py
+++ Building.py
@@ -5,6 +5,7 @@ from typing import List
 class Building(object):
 
     def __init__(self, floors):
-        self.floors: floors
+        self.floors = floors
     
+    floors: List[Floor]
 
--- building_test.py
+++ building_test.py
@@ -9,17 +9,17 @@ class building_test(unittest.TestCase):
     ##Building test
     def test_building_has_floors(self):
         building = Building([Floor(), Floor()])
-        self.assertGreater(len(building.floors), 1)
+        self.assertGreater(len(building.floors), 0)
     
     def test_new_building_floors_start_at_0(self):
-        building = Building([Floor()])
-        self.assertEqual(len(building.floors[0]), 0)
+        building = Building([Floor(0)])
+        self.assertEqual(building.floors[0], 0)
 
     def test_each_floor_has_unique_number(self):
         building = Building([Floor(), Floor()])
         self.assertEqual(len(set(building.floors)), len(building.floors))
 
-    def test_error_raised_when_not_enough_floors(self):
+    def test_error_raised_when_no_floors(self):
         with self.assertRaises(AssertionError):
             Building(Floor(None))
 
