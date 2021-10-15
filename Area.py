9746d052662545a7a44305d4552c22b3182d35da
Sarah Issaad <sarah.issaad.etu@univ-lille.fr>
2021-10-15 14:35:14 +0200

    An area must have at least one wall.

--- Area.py
+++ Area.py
@@ -5,5 +5,8 @@ from Floor import Floor
 
 class Area(object):
 
-    def __init__(self, floor):
+    def __init__(self, floor, wall=[]):
         assert(isinstance(floor, Floor)), "The floor parameter must be a Floor object."
+        assert(isinstance(wall, list)), "The wall parameter must be a list."
+        self.nbWalls = len(wall)
+        assert(self.nbWalls > 0), "There must be at least one wall in the Area."
--- area_test.py
+++ area_test.py
@@ -1,13 +1,17 @@
 from Area import Area
 from Floor import Floor
+from Wall import Wall
 from typing import List
 import unittest
 
+class mock_area(Area):
+    def __init__(self, floor, wall=[]):
+        super().__init__(floor, wall)
 
 class area_test(unittest.TestCase):
 
     def test_unique_floor_ok(self):
-        Area(Floor())
+        Area(Floor(), wall=[Wall([(0,1), (0,2)])])
     
     def test_error_when_no_unique_floor(self):
         floor1 = Floor()
@@ -15,7 +19,14 @@ class area_test(unittest.TestCase):
         with self.assertRaises(AssertionError):
             Area(floor=[floor1, floor2])
 
+    def test_area_one_wall_ok(self):
+        area = mock_area(floor=Floor(), wall=[Wall([(0,1), (0,2)])])
+        self.assertTrue(area.nbWalls == 1)
 
+    def test_area_no_wall_error(self):
+        with self.assertRaises(AssertionError):
+            area = mock_area(floor=Floor())
+        
 
     if __name__ =='__main__':
         pass
