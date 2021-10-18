7f6dbb59e257fef3d8af2a53c469dbfd64a36592
Sarah Issaad <sarah.issaad.etu@univ-lille.fr>
2021-10-18 09:02:24 +0200

    Deleted tests in floor_test that are now in building_test, deleted an assert in element test that didn't work.

--- element_test.py
+++ element_test.py
@@ -17,7 +17,6 @@ class element_test(unittest.TestCase):
     def test_element_raises_error_when_no_coordinate(self):
         with self.assertRaises(AssertionError):
             elt = mock_element([])
-        self.assertEqual("Element has no coordinates.")
 
     if __name__ =='__main__':
         pass
